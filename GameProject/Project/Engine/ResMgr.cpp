62e2a9b0554348044de1fdaccc7bf1db9ab2dab8
Bronze2 <leedongsu97@naver.com>
2022-04-13 16:46:17 +0900

    modify

--- GameProject/Project/Engine/ResMgr.cpp
+++ GameProject/Project/Engine/ResMgr.cpp
@@ -656,7 +656,7 @@ void CResMgr::CreateDefaultShader()
 	pShader->CreateVertexInstShader(L"Shader\\std3d.fx", "VS_Std3D_Inst", "vs_5_0");
 	pShader->CreatePixelShader(L"Shader\\std3d.fx", "PS_Std3D", "ps_5_0");
 
-	pShader->Create(SHADER_POV::FORWARD);
+	pShader->Create(SHADER_POV::DEFERRED);
 	AddRes(L"Std3DShader", pShader);
 
 	// =============
