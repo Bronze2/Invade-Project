d7809a12ee46a2bbd79d2b647be9b0a231a356e7
Bronze2 <leedongsu97@naver.com>
2022-05-13 03:57:38 +0900

    Update pch.h

--- GameProject/Project/Engine/pch.h
+++ GameProject/Project/Engine/pch.h
@@ -10,19 +10,55 @@
 #include <SDKDDKVer.h>
 #define WIN32_LEAN_AND_MEAN   
 
+
+
 // 여기에 미리 컴파일하려는 헤더 추가
 
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <Windows.h>
-
 #include "global.h"
+#include "d3dUtil.h";
+#include <WS2tcpip.h>
+#include <thread>
+#include <mutex>
+#include <iostream>
+#include <unordered_map>
+#include <chrono>
 
+#include <dxgi1_4.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+// D2D (for Render Text)
+#include <d2d1_3.h>
+#include <dwrite.h>
+#include <d3d11on12.h>
+
+
+
+#pragma comment(lib, "d2d1.lib")
+#pragma comment(lib, "dwrite.lib")
+#pragma comment(lib, "d3d11.lib")
+
+#pragma comment (lib, "WS2_32.lib")
+#pragma comment (lib, "mswsock.lib")
 
 // 콘솔창 띄우기
 
-//#ifdef UNICODE
-//#pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:console") 
-//#else
-//#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console") 
-//#endif
+#ifdef UNICODE
+#pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:console") 
+#else
+#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console") 
+#endif
+
+using namespace std;
+using namespace chrono;
 
 #endif //PCH_H
